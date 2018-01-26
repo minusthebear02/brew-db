d13f08b4af6837bc02884455e16789a9e82862d4
Mark Froehlich <mfroeh2@gmail.com>
2018-01-26 08:12:54 -0500

    Name Change

--- src/App.js
+++ src/App.js
@@ -2,42 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let arr = [];
-
 class App extends Component {
-
-
-  submit = () => {
-    arr.push({ name: this.text.value, email: this.email.value })
-    console.log(arr);
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React, Brah!" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input type="text" ref={(input) => this.text = input} />
-        <input type="email" ref={(input) => this.email = input} />
-        <button onClick={this.submit}>Show Value</button>
+
       </div>
     );
   }
 
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <h1 className="App-title">{text}</h1>
-    );
-  }
 }
 
 export default App;
